7bdc5d4bce6ddf10347d56b635a02abead36bfa3
mdolata <mateuszdolata2@gmail.com>
2019-06-13 22:43:11 +0200

    remove unneeded returns from dao

--- src/main/java/com/mdolata/urlCutter/dao/DAO.kt
+++ src/main/java/com/mdolata/urlCutter/dao/DAO.kt
@@ -6,27 +6,20 @@ import arrow.core.toOption
 class PairDAO {
     private val list = ArrayList<Pair>()
 
-    fun addNewPair(url: String, cutURL: String) {
-        list.add(Pair(url, cutURL))
-    }
+    fun addNewPair(url: String, cutURL: String) = list.add(Pair(url, cutURL))
 
-    fun getPairOf(url: String): Option<Pair> {
-        return Option.fromNullable(list.find { x -> x.url == url })
-    }
 
-    fun getUrl(url: String): Option<String> {
-        return list.find { x -> x.url == url }
-                .toOption()
-                .map { pair -> pair.url }
-    }
+    fun getPairOf(url: String): Option<Pair> = Option.fromNullable(list.find { x -> x.url == url })
 
-    fun getCutURL(cutURL: String): Option<String> {
-        return list.find { x -> x.cutURL == cutURL }
-                .toOption()
-                .map { pair -> pair.cutURL }
-    }
+    fun getUrl(url: String): Option<String> = list.find { x -> x.url == url }
+            .toOption()
+            .map { pair -> pair.url }
 
-    fun getAll(): List<Pair> {
-        return list.toList()
-    }
+
+    fun getCutURL(cutURL: String): Option<String> = list.find { x -> x.cutURL == cutURL }
+            .toOption()
+            .map { pair -> pair.cutURL }
+
+
+    fun getAll(): List<Pair> = list
 }
